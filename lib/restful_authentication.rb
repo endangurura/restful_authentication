a6f08508270441dc561985d477a0500b9280d0a6
Emmanuel Ndangurura <endangurura@tachyinc.com>
2021-04-28 21:10:55 +0300

    first commit

---
+++ lib/restful_authentication.rb
@@ -0,0 +1,6 @@
+require "restful_authentication/version"
+
+module RestfulAuthentication
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/restful_authentication/version.rb
@@ -0,0 +1,3 @@
+module RestfulAuthentication
+  VERSION = "0.1.0"
+end
